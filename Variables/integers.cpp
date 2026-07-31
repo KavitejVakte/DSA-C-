c83bee25dbdecac3e902b9d5b2d21e6dcb24f078
KavitejVakte <kavitejvakte12@gmail.com>
2026-07-31 22:16:30 +0530

    added Variables

---
+++ Variables/integers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;   // decleartion
+    x=8;    // initialization
+    cout<<x<<"\n";
+    cout<<x+12<<endl;
+    int a=5,b=5;
+    cout<<a+b;
+} 
