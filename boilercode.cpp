96d11988be02df3760ab4bfb34ac288cb576f15b
KavitejVakte <kavitejvakte12@gmail.com>
2026-07-31 17:38:18 +0530

    added a code

--- boilercode.cpp
+++ boilercode.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main(){
     // yaha pe saraa code likha jayega as of now 
     cout<<"hello kavitej welcome to Coding ";
+    cout<<endl;
+    cout<<"its great that you are doing DSA in C++";
     return 0 ;
 }
 
