335dc34c884d7798f1dbe26e759bfc593d9e19a4
KavitejVakte <kavitejvakte12@gmail.com>
2026-08-06 22:12:25 +0530

    added square_no & no_input

---
+++ Assignments/no_input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter first and second number:";
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<"the addition of the number is "<<endl;
+    cout<<a+b;
+    return 0;
+
+}
