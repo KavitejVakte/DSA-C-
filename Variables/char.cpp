59b4cb9a7ec644ba831ef615194936c4cc75fcc6
KavitejVakte <kavitejvakte12@gmail.com>
2026-08-08 20:29:46 +0530

    added char

---
+++ Variables/char.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    char x ='#';  
+    cout<<x<<"\n";
+
+    // ascii value printing
+    char d='B';
+    cout<<(int)d<<"\n";
+    //vice-versa
+
+    int a=75;
+    cout<<(char)a<<"\n";
+
+    return 0;
+
+}
